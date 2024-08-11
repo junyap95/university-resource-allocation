1f73748afdca703eb282bce4f43345946fcf6d6d
Jun <63500365+junyap95@users.noreply.github.com>
2024-08-11 18:01:24 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://localhost:3000/view-tables",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
 });
