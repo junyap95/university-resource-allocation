24930ea4fab00b2c1cec5c253f62ec650a83ceac
Jun <63500365+junyap95@users.noreply.github.com>
2024-08-11 18:12:15 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -73,6 +73,6 @@ export default defineConfig({
   webServer: {
     command: "npm run start",
     url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 });
